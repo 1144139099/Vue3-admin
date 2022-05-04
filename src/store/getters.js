eddc097a3b059c9b7df5c9d3332eba05f89c3416
hlh <1144139099@qq.com>
2022-05-04 20:26:08 +0800

    登录鉴权解决方案

---
+++ src/store/getters.js
@@ -0,0 +1,13 @@
+const getters = {
+  token: state => state.user.token,
+  userInfo: state => state.user.userInfo,
+  /**
+   * @returns true 表示已存在用户信息
+   */
+  hasUserInfo: state => {
+    return JSON.stringify(state.user.userInfo) !== '{}'
+  },
+  cssVar: () => variables,
+  sidebarOpened: (state) => state.app.sidebarOpened
+}
+export default getters
