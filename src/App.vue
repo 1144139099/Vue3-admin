93214459c232ea4d82d325044c7e42639b0607c0
hlh <1144139099@qq.com>
2022-05-05 20:20:27 +0800

    修复刷新页面后，新主题会失效的问题

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
   <router-view />
 </template>
 
-<script>
-export default {
-  name: 'App'
-}
-</script>
+<script setup>
+import { useStore } from 'vuex'
+import { generateNewStyle, writeNewStyle } from '@/utils/theme'
 
+const store = useStore()
+generateNewStyle(store.getters.mainColor).then(newStyleText => {
+  writeNewStyle(newStyleText)
+})
+</script>
 <style></style>
